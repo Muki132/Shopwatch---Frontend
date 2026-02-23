a4b1daaf7d524fd53c51e381ed977e77a31bc7dc
Deepak <dknayak1329@gmail.com>
2026-02-23 22:53:55 +0530

    Frontend code push

---
+++ src/components/checkoutAndOrder/Orders/Seller.jsx
@@ -0,0 +1,13 @@
+const sellerDetails = [
+    {
+      name:"Deepak Ku Nayak",
+      address:"Banpur",
+      state:"Odisha",
+      city:"Balugaon",
+      zipCode:"752030",
+      email:"dknayak1329@gmail.com",
+      phone:7008454980
+  
+    }
+  ]
+export default sellerDetails  
